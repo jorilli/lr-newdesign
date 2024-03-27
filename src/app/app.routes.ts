a75d37ff95dc5aff63a3e1f39e8560e1da07bb6f
Jori Villiger <jori.villiger@swisscom.com>
2024-03-27 16:10:34 +0100

    header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomepageComponent} from "./homepage/homepage.component";
+import {ContactComponent} from "./contact/contact.component";
+import {OffersComponent} from "./offers/offers.component";
+import {BiographyComponent} from "./biography/biography.component";
+import {PortfolioComponent} from "./portfolio/portfolio.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'offers', component: OffersComponent},
+  {path: 'biography', component: BiographyComponent},
+  {path: 'portfolio', component: PortfolioComponent}
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatButton,
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
